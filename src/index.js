97a09f3a69811caf626478d43b5128660da335c1
Marcela Neira <martaymarcela@gmail.com>
2021-01-02 19:58:53 +0100

    jsx y React

--- src/index.js
+++ src/index.js
@@ -6,13 +6,22 @@ container.appendChild(element); */
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello, Platzi </h1>;
+/* const element = <h1>Hello, Platzi </h1>; */
+
+const jsx=(
+<div>
+    <h1>Hola, Soy Marcela Neira</h1>
+    <p>Soy Desarrolladora Web</p>
+</div>
+);
+
+const element = React.createElement(
+    'div',
+    {},
+    React.createElement('h1',{},'Hola Soy Marcela'),
+    React.createElement('p',{},'Soy Ama de casa')
+)
 const container = document.getElementById('app');
 
 //ReactDOM.render(__qué__, __donde__)
 ReactDOM.render(element,container)
-
-
-
-
-
